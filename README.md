b3cf29fab33c378da2e1b30bff14b9638c16d422
Nicholas Judd <nickkjudd@gmail.com>
2023-02-14 08:40:21 +0000

    version 0.0.1

---
+++ README.md
@@ -0,0 +1,115 @@
+[![R-CMD-check](https://github.com/njudd/ggrain/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/njudd/ggrain/actions/workflows/R-CMD-check.yaml)
+[![Bugs](https://img.shields.io/github/issues/njudd/ggrain/bug?label=Bugs&logo=github&logoColor=%23FFF&color=brightgreen)](https://github.com/njudd/ggrain/issues?q=is%3Aopen+is%3Aissue)
+[![CRAN_Release_Badge](http://www.r-pkg.org/badges/version-ago/ggrain)](https://CRAN.R-project.org/package=ggrain)
+[![CRAN_Download_Badge](http://cranlogs.r-pkg.org/badges/grand-total/ggrain)](https://CRAN.R-project.org/package=ggrain)
+[![](http://cranlogs.r-pkg.org/badges/ggrain)](https://cran.r-project.org/package=ggrain)
+[![Vignette](https://img.shields.io/badge/Vignette-ggrain-orange.svg?colorB=E91E63)](https://www.njudd.com/raincloud-ggrain/)
+<!---[![License: ]()](https://github.com/njudd/ggrain/LICENSE)--->
+
+# `ggrain` - [Raincloud Plots](https://wellcomeopenresearch.org/articles/4-63/v2)
+
+`ggrain` is an R-package that allows you to create Raincloud plots - following the 'Grammar of Graphics' (i.e., ggplot2) - that are: 
+
+- Highly customizable
+- Connect longitudinal observations
+- Handles Likert data
+- Allows mapping of a covariate.
+	
+### Example 
+
+```r
+ggplot(iris, aes(x = 1, y = Sepal.Length)) +
+  geom_rain()
+```
+
+### Installation
+
+```r
+if (!require(remotes)) {
+    install.packages("remotes")
+}
+remotes::install_github('njudd/ggrain')
+
+library(ggrain)
+```
+
+###  Simple examples
+
+1.  Raincloud per group
+
+	```r
+	ggplot(iris, aes(x = Species, y = Sepal.Length, fill = 	Species)) +
+		geom_rain(rain.side = 'l')
+	```
+
+2.  Different groups overlapped
+
+	```r
+	ggplot(iris, aes(x = 1, y = Sepal.Length, fill = Species)) +
+		geom_rain(alpha = .5)
+	```
+
+
+![img](https://raw.githubusercontent.com/njudd/ggrain/main/inst/git_pics/basic_rain.png)
+
+### Vignette
+For a complete overview of `ggrain` such as a 2-by-2 raincloud plot or multiple repeated measures, please see our [Vignette](https://www.njudd.com/raincloud-ggrain/).
+
+### `ggrain` specific features
+
+`geom_rain` is a combination of 4 different ggplot2 geom's (i.e., point, line, boxplot & violin).
+
+- `id.long.var`: a grouping variable to connect the lines by
+- `cov`: a covariate to remap the color of the points
+- `Likert`: `True` or `False` response which adds y jittering
+- `rain.side`: Which side to display the rainclouds: 'l' for left, 'r' for right and 'f' for flanking
+
+Specific geom arguments can be passed with a list to any of the 4 geom's with the argument `{point/line/boxplot/violin}.args`. For a list of arguments that can be passed see the help files of the respective geom's (e.g., `?gghalves::geom_half_violin`).
+
+Position-related arguments (e.g., jittering, nudging & width) can be passed with `{point/line/boxplot/violin}.args.pos`, see the help file of `?geom_rain` for defaults
+
+![img](https://raw.githubusercontent.com/njudd/ggrain/main/inst/git_pics/time_group_cov_vin.png)
+
+### Contributions / Issues
+
+We warmly welcome all contributions. 
+You can open an issue or make a pull request if you would like to add something new!
+
+### Citation
+
+[`ggrain`](https://github.com/njudd/ggrain) was developed by Nicholas Judd, Jordy van Langen and Rogier Kievit. 
+
+<pre>
+- Judd, Nicholas, van Langen, Jordy, & Kievit, Rogier.
+    <i>ggrain: a ggplot2 extension package to create Raincloud Plots in R.</i>
+    <b>GitHub</b> 2023,
+    <a href="https://github.com/njudd/ggrain">https://github.com/njudd/ggrain</a>
+</pre>
+
+### Funding
+<img src="https://github.com/njudd/ggrain/blob/main/inst/git_pics/nwo_openscience.jpg" width="150" height="160" align="right"/>
+
+In 2021, NWO (Dutch research council) announced their inaugural [NWO Open Science Fund](https://www.nwo.nl/en/researchprogrammes/open-science/open-science-fund). The Open Science Fund aims to support researchers to develop, test and implement innovative ways of making research open, accessible, transparent and reusable, covering the whole range of Open Science. The Raincloud plots team was awarded this fantastic initiative and is specifically working on:
+
+- Creating the [`ggrain`](https://github.com/njudd/ggrain) R-package
+- Creating a ShinyApp Raincloudplots
+- Integrating Raincloudplots in [JASP Statistics](https://jasp-stats.org)
+- Organzing [globally accessible, online workshops](https://github.com/jorvlan/raincloudplots-workshops) to help people create raincloudplots and improve their data visualizations in general.
+
+You can read more about our awarded project here: https://www.nwo.nl/en/projects/203001011 or you can watch the online webinar hosted by NWO about our project: [![Webinar Open Science series S1E2: Open tools for data enrichment and visualization](https://github.com/njudd/ggrain/blob/main/inst/git_pics/raincloudplots_NWO_webinar.png)](https://youtu.be/Kvcyh_9KSbw?t=1910 "Webinar Open Science series S1E2: Open tools for data enrichment and visualization")
+
+
+### Raincloud Plots 
+
+**Paper**
+<br>
+<pre>
+- Allen, M., Poggiali, D., Whitaker, K., Marshall, T. R., van Langen, J., & Kievit, R. A.
+    Raincloud plots: a multi-platform tool for robust data visualization [version 2; peer review: 2 approved] 
+    <b>Wellcome Open Research</b> 2021, 4:63. <a href="https://doi.org/10.12688/wellcomeopenres.15191.2">https://doi.org/10.12688/wellcomeopenres.15191.2</a>
+</pre>
+
+There are now ***3*** ways in which you can use our Raincloud Plots tools: 
+- through a series of specific easy to modify scripts [https://github.com/RainCloudPlots/RainCloudPlots](https://github.com/RainCloudPlots/RainCloudPlots)
+- through our initial [`raincloudplots`](https://github.com/jorvlan/raincloudplots) package
+- through the newest R-package [`ggrain`](https://github.com/njudd/ggrain)
---
+++ inst/doc/ggrain.R
@@ -0,0 +1,178 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  #warning = FALSE, message = FALSE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(ggrain)
+
+## ----most basic raincloud possible--------------------------------------------
+ggplot(iris, aes(1, Sepal.Width)) +
+  geom_rain() +
+  theme_classic() +
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), axis.ticks.x = element_blank())
+
+## ----rainclouds by species grouped--------------------------------------------
+ggplot(iris, aes(1, Sepal.Width, fill = Species)) +
+  geom_rain(alpha = .5) +
+  theme_classic() +
+  scale_fill_brewer(palette = 'Dark2')
+
+## ----rainclouds by Species colored--------------------------------------------
+ggplot(iris, aes(1, Sepal.Width, fill = Species, color = Species)) +
+  geom_rain(alpha = .6,
+            boxplot.args = list(color = "black", outlier.shape = NA)) +
+  theme_classic() +
+  scale_fill_brewer(palette = 'Dark2') +
+  scale_color_brewer(palette = 'Dark2')
+
+## ----rainclouds by Species colored dogded-------------------------------------
+ggplot(iris, aes(1, Sepal.Width, fill = Species, color = Species)) +
+  geom_rain(alpha = .5, rain.side = 'l',
+            boxplot.args = list(color = "black", outlier.shape = NA),
+            boxplot.args.pos = list(
+              position = ggpp::position_dodgenudge(x = .1), width = 0.1
+            )) +
+  theme_classic() +
+  scale_fill_brewer(palette = 'Dark2') +
+  scale_color_brewer(palette = 'Dark2') +
+  guides(fill = 'none', color = 'none')
+
+## ----rainclouds by species----------------------------------------------------
+ggplot(iris, aes(Species, Sepal.Width, fill = Species)) +
+  geom_rain(alpha = .5) +
+  theme_classic() +
+  scale_fill_brewer(palette = 'Dark2') +
+  guides(fill = 'none', color = 'none')
+
+## ----rainclouds by time flip--------------------------------------------------
+ggplot(iris, aes(Species, Sepal.Width, fill = Species)) +
+  geom_rain(alpha = .5) +
+  theme_classic() +
+  scale_fill_brewer(palette = 'Dark2') +
+  guides(fill = 'none', color = 'none') +
+  coord_flip()
+
+## ----rainclouds by time flip nudged-------------------------------------------
+ggplot(iris, aes(Species, Sepal.Width, fill = Species)) +
+  geom_rain(alpha = .5, 
+            boxplot.args.pos = list(
+              width = 0.05, position = position_nudge(x = 0.13)),
+            violin.args.pos = list(
+              side = "r",
+              width = 0.7, position = position_nudge(x = 0.2))) +
+  theme_classic() +
+  scale_fill_brewer(palette = 'Dark2') +
+  guides(fill = 'none', color = 'none') +
+  coord_flip()
+
+## ----rainclouds by time colored by group--------------------------------------
+ggplot(iris, aes(Species, Sepal.Width, fill = Species)) +
+  geom_rain(alpha = .6,
+            cov = "Sepal.Length") +
+  theme_classic() +
+  scale_fill_brewer(palette = 'Dark2') +
+  guides(fill = 'none', color = 'none') +
+  scale_color_viridis_c(option =  "A", direction = -1)
+
+## ----making long iris data----------------------------------------------------
+set.seed(42) # the magic number
+
+iris_subset <- iris[iris$Species %in% c('versicolor', 'virginica'),]
+
+iris.long <- cbind(rbind(iris_subset, iris_subset, iris_subset), 
+                   data.frame(time = c(rep("t1", dim(iris_subset)[1]), rep("t2", dim(iris_subset)[1]), rep("t3", dim(iris_subset)[1])),
+                              id = c(rep(1:dim(iris_subset)[1]), rep(1:dim(iris_subset)[1]), rep(1:dim(iris_subset)[1]))))
+
+# adding .5 and some noise to the versicolor species in t2
+iris.long$Sepal.Width[iris.long$Species == 'versicolor' & iris.long$time == "t2"] <- iris.long$Sepal.Width[iris.long$Species == 'versicolor' & iris.long$time == "t2"] + .5 + rnorm(length(iris.long$Sepal.Width[iris.long$Species == 'versicolor' & iris.long$time == "t2"]), sd = .2)
+# adding .8 and some noise to the versicolor species in t3
+iris.long$Sepal.Width[iris.long$Species == 'versicolor' & iris.long$time == "t3"] <- iris.long$Sepal.Width[iris.long$Species == 'versicolor' & iris.long$time == "t3"] + .8 + rnorm(length(iris.long$Sepal.Width[iris.long$Species == 'versicolor' & iris.long$time == "t3"]), sd = .2)
+
+# now we subtract -.2 and some noise to the virginica species
+iris.long$Sepal.Width[iris.long$Species == 'virginica' & iris.long$time == "t2"] <- iris.long$Sepal.Width[iris.long$Species == 'virginica' & iris.long$time == "t2"] - .2 + rnorm(length(iris.long$Sepal.Width[iris.long$Species == 'virginica' & iris.long$time == "t2"]), sd = .2)
+
+# now we subtract -.4 and some noise to the virginica species
+iris.long$Sepal.Width[iris.long$Species == 'virginica' & iris.long$time == "t3"] <- iris.long$Sepal.Width[iris.long$Species == 'virginica' & iris.long$time == "t3"] - .4 + rnorm(length(iris.long$Sepal.Width[iris.long$Species == 'virginica' & iris.long$time == "t3"]), sd = .2)
+
+iris.long$Sepal.Width <- round(iris.long$Sepal.Width, 1) # rounding Sepal.Width so t2 data is on the same resolution
+iris.long$time <- factor(iris.long$time, levels = c('t1', 't2', 't3'))
+
+## ----rainclouds by time by group----------------------------------------------
+ggplot(iris.long[iris.long$time %in% c('t1', 't2'),], aes(time, Sepal.Width, fill = Species)) +
+  geom_rain(alpha = .5) +
+  theme_classic() +
+  scale_fill_manual(values=c("dodgerblue", "darkorange")) +
+  guides(fill = 'none', color = 'none')
+
+## ----rainclouds by time by group flipped--------------------------------------
+ggplot(iris.long[iris.long$time %in% c('t1', 't2'),], aes(time, Sepal.Width, fill = Species)) +
+  geom_rain(alpha = .5, rain.side = 'f',
+             boxplot.args.pos = list(width = .1,
+                position = ggpp::position_dodgenudge(
+                  x = c(-.13, -.13, # pre versicolor, pre virginica
+                        .13, .13))), # post; post
+            violin.args.pos = list(width = .7,
+             position = position_nudge(x = c(rep(-.2, 256*2), rep(-.2, 256*2),# pre; pre
+                                             rep(.2, 256*2), rep(.2, 256*2))))) + #post; post
+  theme_classic() +
+  scale_fill_manual(values=c("dodgerblue", "darkorange")) +
+  guides(fill = 'none', color = 'none')
+
+## ----rainclouds by time by group flipped long id------------------------------
+ggplot(iris.long[iris.long$time %in% c('t1', 't2'),], aes(time, Sepal.Width, fill = Species)) +
+  geom_rain(alpha = .5, rain.side = 'f2x2', id.long.var = "id") +
+  theme_classic() +
+  scale_fill_manual(values=c("dodgerblue", "darkorange")) +
+  guides(fill = 'none', color = 'none')
+
+## ----rainclouds by time by group flipped long id colored----------------------
+ggplot(iris.long[iris.long$time %in% c('t1', 't2'),], aes(time, Sepal.Width, fill = Species, color = Species)) +
+  geom_rain(alpha = .5, rain.side = 'f2x2', id.long.var = "id",
+            violin.args = list(color = NA, alpha = .7)) +
+  theme_classic() +
+  scale_fill_manual(values=c("dodgerblue", "darkorange")) +
+  scale_color_manual(values=c("dodgerblue", "darkorange")) +
+  guides(fill = 'none', color = 'none')
+
+## ----rainclouds long cov------------------------------------------------------
+ggplot(iris.long, aes(time, Sepal.Width, fill = Species)) +
+  geom_rain(alpha = .5, rain.side = 'f', id.long.var = "id", cov = "Sepal.Length",
+            boxplot.args = list(outlier.shape = NA, alpha = .8),
+            violin.args = list(alpha = .8, color = NA),
+            boxplot.args.pos = list(width = .1,
+             position = ggpp::position_dodgenudge(x = c(-.13, -.13, # t1 old, t1 young
+                                                        -.13, .13, 
+                                                         .13, .13))),
+            violin.args.pos = list(width = .7,
+             position = position_nudge(x = c(rep(-.2, 256*2), rep(-.2, 256*2),# t1
+                                             rep(-.2, 256*2), rep(.2, 256*2), # t2
+                                             rep(.2, 256*2), rep(.2, 256*2))))) +
+  theme_classic() +
+  scale_fill_manual(values=c("dodgerblue", "darkorange")) +
+  scale_color_viridis_c(option =  "A", direction = -1) +
+  guides(fill = 'none', color = 'none')
+
+## ----rainclouds by time by group flipped long id colored mean-----------------
+ggplot(iris.long[iris.long$time %in% c('t1', 't2'),], aes(time, Sepal.Width, fill = Species)) +
+  geom_rain(alpha = .5, rain.side = 'f2x2') +
+  theme_classic() +
+  stat_summary(fun = mean, geom = "line", aes(group = Species, color = Species)) +
+  stat_summary(fun = mean, geom = "point",
+               aes(group = Species, color = Species)) +
+  scale_fill_manual(values=c("dodgerblue", "darkorange")) +
+  scale_color_manual(values=c("dodgerblue", "darkorange")) +
+  guides(fill = 'none', color = 'none')
+
+## ----rainclouds sig, eval = F, echo = T---------------------------------------
+#  ggplot(iris.long[iris.long$Species == 'versicolor' & iris.long$time %in% c('t1', 't2'),], aes(time, Sepal.Width, fill = Species)) +
+#    geom_rain(alpha = .5, rain.side = 'f1x1') +
+#   ggsignif::geom_signif(
+#    comparisons = list(c("t1", "t2")),
+#    map_signif_level = TRUE) +
+#  scale_fill_manual(values=c("darkorange", "darkorange")) +
+#  theme_classic()
+
